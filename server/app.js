cbb33edea991f2a430868a6dcff6e638010578fe
shankruganganahalli003-cpu <shankruganganahalli003@gmail.com>
2026-02-22 09:48:11 +0530

    fix nodemon production issue

--- server/app.js
+++ server/app.js
@@ -1,33 +1,35 @@
+require("dotenv").config();
+
 const express = require("express");
-const app = express()
+const app = express();
 const connectDB = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-require("dotenv").config();
+
 const authrouter = require("./routes/authroute");
 const studentrouter = require("./routes/studentroute");
 const teacherrouter = require("./routes/teacherroute");
 const subjectrouter = require("./routes/subjectroute");
 const marksrouter = require("./routes/marksroute");
 
-
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({origin:"http://localhost:5173",
-    credentials:true
+
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
 }));
 
+app.use("/api/auth", authrouter);
+app.use("/api/student", studentrouter);
+app.use("/api/teacher", teacherrouter);
+app.use("/api/subject", subjectrouter);
+app.use("/api/marks", marksrouter);
 
-app.use("/api/auth",authrouter);
-app.use("/api/student",studentrouter);
-app.use("/api/teacher",teacherrouter);
-app.use("/api/subject",subjectrouter);
-app.use("/api/marks",marksrouter);
+const PORT = process.env.PORT || 3000;
 
-
-const port = process.env.MONGO_URI
-app.listen(port,()=>{
-    connectDB();
-    console.log(`server is running ${port}`)
-})
+app.listen(PORT, async () => {
+  await connectDB();
+  console.log(`Server running on ${PORT}`);
+});
